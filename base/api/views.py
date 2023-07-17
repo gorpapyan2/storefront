3432b0198a062abddbbdc80679e5f9e97728371b
gorpapyan2 <gorpapyan2@gmail.com>
2023-07-17 14:49:03 +0400

    ----

---
+++ base/api/views.py
@@ -0,0 +1,28 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerailizer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'GET /api',
+        'GET /api/rooms',
+        'GET /api/rooms/:id'
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerailizer(rooms, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    rooms = Room.objects.get(id=pk)
+    serializer = RoomSerailizer(rooms, many=False)
+    return Response(serializer.data)
